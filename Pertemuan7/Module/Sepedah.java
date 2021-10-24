8335ab99ce8d2d0d5319111a57a289bdad356c31
Aryahira <aryahirawansyah@gmail.com>
2021-10-24 17:16:04 +0700

    Pertemuan 7

--- Pertemuan7/Module/Sepedah.java
+++ Pertemuan7/Module/Sepedah.java
@@ -1,83 +1,47 @@
-class Sepedah { //main class
-    public String nama; 
+class Sepedah {
+    public String merk; 
+    public String stang;
+    public String jok;
+    public String gear;
+    public String ban;
 
-    public void setNama(String newNama) {
-        this.nama = newNama;
+    public String getMerk() {
+        return merk;
     }
 
-    public String getNama() {
-        return nama;
+    public void setMerk(String newMerk) {
+        this.merk = newMerk;
     }
-
-    String merk() { 
-        return "Sepedah merek Polygon";
-    }
-
-}
-class Stang extends Sepedah {
-    public int stang;
     
-    public int getStang() {
+    public String getStang() {
         return stang;
     }
 
-    public void setStang(int newStang) {
+    public void setStang(String newStang) {
         this.stang = newStang;
     }
-}
 
-class Jok extends Sepedah {
-    public String jok;
-    
     public String getJok() {
         return jok;
     }
+        
     public void setJok(String newJok) {
         this.jok = newJok;
-    }     
-}
-
-class Gear extends Sepedah {
-    public int gear;
-
-    public int getGear() {
+    }
+    
+    public String getGear() {
         return gear;
     }
-    public void setGear(int newGear) {
+
+    public void setGear(String newGear) {
         this.gear = newGear;
     }
-}
 
-class Ban extends Sepedah {
-    public int ban;
-
-    public int getBan() {
+    public String getBan() {
         return ban;       
     }
-    public void setBan(int newBan) {
+
+    public void setBan(String newBan) {
         this.ban = newBan;
-    }
-}
-
-class Hasil{
-    public static void main(String[] args) {
-        Sepedah mySepedah = new Sepedah();
-        System.out.println(mySepedah.merk());
-
-        Stang myStang = new Stang();
-        myStang.setStang(13);
-        System.out.println("Panjang stang: " + myStang.getStang()+ " cm");
-
-        Jok myJok = new Jok();
-        myJok.setJok("Jok ini sangat empuk");
-        System.out.println(myJok.getJok());
-
-        Gear myGear = new Gear();
-        myGear.setGear(52);
-        System.out.println("Ukuran Gear " + myGear.getGear()+ " cm");
-
-        Ban myBan = new Ban();
-        myBan.setBan(32);
-        System.out.println("Ukuran ban " + myBan.getBan()+ " mm");
-    }
+    }   
 }
